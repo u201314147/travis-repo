6c69c3d013805b5fdb92d47b0f7e137ec714f4d0
u201314147 <u201314147@upc.edu.pe>
2020-11-21 07:01:06 -0500

    cambios mysql

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'mysql',
-  url: 'mysql://root:mysql@2.tcp.ngrok.io:19648/examen',
+  url: 'mysql://root:mysql@0.tcp.ngrok.io:13370/examen',
   migrations: ['dist/app/infra/migrations/*.js', 'app/infra/migrations/*.js'],
   cli: {
     migrationsDir: './app/infra/migrations',
